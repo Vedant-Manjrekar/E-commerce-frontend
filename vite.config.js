97d7344f5ae27f5e16ab4754e00edf180f40bb8b
Vedant Manjrekar <vedant.28m@gmail.com>
2022-12-15 17:35:49 +0530

    Fixing routing issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-devServer: {
-  historyApiFallback: true;
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  devServer: {
+    historyApiFallback: true,
+  },
 });
